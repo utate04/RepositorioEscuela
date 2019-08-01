0f39d609b89a4e41b3607117dd77f5012a2bd2f0
Ashly Utate <ashleyutate44@gmail.com>
2019-08-01 03:42:30 -0400

    Creacion de la carpeta Persistence- Configuracion de Dbcontext y sus allegados

--- Escuela Api/Escuela Api/Startup.cs
+++ Escuela Api/Escuela Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,7 +32,7 @@ namespace Escuela_Api
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<Controllers___Copy.EscuelaDbContext>(opciones => opciones.UseSqlServer("Data Source=DESKTOP-71GJ61V\\SQLEXPRESS;Initial Catalog=crudescuela;Integrated Security=True"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
