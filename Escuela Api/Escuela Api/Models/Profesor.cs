07811d49ea35dafdbdb1a4ab6c5f9863a6be1c99
Ashly Utate <ashleyutate44@gmail.com>
2019-08-01 02:13:23 -0400

    Creacion de Mapeos

--- Escuela Api/Escuela Api/Models/Profesor.cs
+++ Escuela Api/Escuela Api/Models/Profesor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +14,17 @@ namespace Escuela_Api.Models
         public string nombre { get; set; }
         public string apellido { get; set; }
         public List<Materia> Materias { get; set; }
+
+        public class Map
+        {
+            public Map(EntityTypeBuilder<Profesor> ebProfesor)
+            {
+                ebProfesor.HasKey(x => x.idprofesor);
+                ebProfesor.Property(x => x.nombre).HasColumnName("nombre").HasMaxLength(20);
+                ebProfesor.Property(x => x.apellido).HasColumnName("apellido").HasMaxLength(20);
+
+
+            }
+        }
     }
 }
